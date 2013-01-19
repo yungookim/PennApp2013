0523c9fcf3ef223a3f99495a1ec0db90588b73b0
ec2_kimy <yungookim@gmail.com>
2013-01-19 05:47:47 +0000

    DB insert implementation in dropboxHook

--- dropboxHooks/spine.py
+++ dropboxHooks/spine.py
@@ -1,6 +1,7 @@
 import web
 from dropbox import client, rest, session
-import time
+import time, oauth
+from pymongo import Connection, MongoClient
 
 app_key = '6tqpoonhurv29mz'
 app_secret = 'gepizvnxzc0brtn'
@@ -17,21 +18,32 @@ request_token = sess.obtain_request_token()
 class Index:
 	def GET(self):
 		print "using", app_key, app_secret, access_type
- 		#sess = session.DropboxSession(app_key, app_secret, access_type)
-		#request_token = sess.obtain_request_token()
-	
+ 			
 		if not sess.is_linked():
 			url = sess.build_authorize_url(request_token, 'http://simplyi.me:3000/allowed')
 			raise web.seeother(url)
 		else:
-			return 'This is the index page'
+			return 'Well... okay...'
 
 class Allowed:
 	def GET(self):
 		access_token = sess.obtain_access_token(request_token)
 		allowed_client = client.DropboxClient(sess)
-		return "linked account:", allowed_client.account_info()		
+		print access_token.key, access_token.secret
+		
+		# mongo work here
+		connection = MongoClient()
+		
+		db = connection.simplyime
+		
+		post = allowed_client.account_info()
+		info_datum = db.posts
+		oid = info_datum.insert(post)
+		#new_url = 'http://simplyi.me:3000/allowed?ObjectId=' + str(oid) 
 
+		#raise web.seeother(new_url)		
+		return oid
+		
 
 if __name__ == "__main__":
 	app = web.application(urls, globals())
