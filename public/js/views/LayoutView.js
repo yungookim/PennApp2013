60087607312c0d6bbecf68d7ea02dba160ac918b
James Choi <james.choiboi@gmail.com>
2013-01-19 17:47:21 -0500

    First layout fixed.

--- public/js/views/LayoutView.js
+++ public/js/views/LayoutView.js
@@ -18,20 +18,17 @@ window.LayoutView = Backbone.View.extend({
             self.template = window.Templates.TemplateStyle1 = temp;
             $(self.el).html(Mustache.to_html(self.template, data));
             console.log("template loaded");
-                    var $container = $('#container');   
-        $container.imagesLoaded( function() {
-            console.log("container being masonried")
-            console.log($container);
-            $container.masonry({
-                itemSelector: '.img',
-                columnWidth : 240
-            });         
-        });
-		  });
+                    var $container = $('#container');  
 
+            $container.imagesLoaded( function() {
+                console.log("container being masonried")
+                console.log($container);
+                $container.masonry({
+                    itemSelector: '.img',
+                });         
+            });
+		});
     	}
-
-
     }
 });
 
