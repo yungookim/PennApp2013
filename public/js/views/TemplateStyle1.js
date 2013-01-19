f3c89acb526238ee6931fbcdfa29806705beb1cd
James Choi <james.choiboi@gmail.com>
2013-01-19 07:04:08 -0500

    merging

---
+++ public/js/views/TemplateStyle1.js
@@ -0,0 +1,21 @@
+window.TemplateStyle1 = Backbone.View.extend({
+
+	el : $("#main"),
+
+    initialize: function() {
+    },
+
+    render: function(eventName) {
+    	var self = this;
+    	self.template = window.Templates.TemplateStyle1;
+
+    	if (self.template){
+    		$(this.el).html(self.template);
+    	} else {
+    		window.loadTemplate('TemplateStyle1', function(temp){
+    			self.template = window.Templates.TemplateStyle1 = temp;
+    			$(self.el).html(self.template);
+    		});
+    	}
+    },
+});
