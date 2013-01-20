a0624753c734f9228f2c33ca000eeeb8f02a827b
wil <wilz.wong@mail.utoronto.ca>
2013-01-20 02:47:47 +0100

    graphics api done

--- public/js/views/wraphics.js
+++ public/js/views/wraphics.js
@@ -1,5 +1,12 @@
-function plus_sign(val){
-	console.log(val);
-	return null;
-	//<div class="wraphics"><div class="plus_sign" style="width:$valpx;height:100px;"><div></div><div></div></div></div>
-}
+$(function (){
+	//plus sign
+	$('.plus_sign').wrap('<div class="wraphics" />');
+	$('.plus_sign').append('<div></div><div></div></div></div>');
+	$('.plus_sign').width($('.plus_sign').data('size'));
+	$('.plus_sign').height($('.plus_sign').data('size'));
+	//eqaul sign
+	$('.equal_sign').wrap('<div class="wraphics" />');
+	$('.equal_sign').append('<div></div><div></div></div></div>');
+	$('.equal_sign').width($('.equal_sign').data('size'));
+	$('.equal_sign').height($('.equal_sign').data('size'));
+});
