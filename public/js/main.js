c7340de2fcee2494664bdb2bdc020399d06ac2dd
wil <wilz.wong@mail.utoronto.ca>
2013-01-20 10:36:25 +0100

    nav bar

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 $(function(){
 
@@ -55,57 +54,3 @@ window.loadTemplate = function(name, next){
 		next(data);
 	}, 'text');
 }
-=======
-"use strict";
-$(function(){
-
-	window.Templates = {};
-	window.userModel;
-	window.app = new AppRouter;
-	Backbone.history.start();
-})
-
-//code for the nav bar
-
-
-
-var AppRouter = Backbone.Router.extend({
-
-	routes : {
-        "" : "landing",
-        "layout/:id" : "layout",
-        "sandbox" : "sandbox",
-        "*actions": 'defaultAction'
-	},
-
-	landing : function(){
-		(new LandingView()).render();
-	},
-
-	layout : function(id) {
-		var self = this;
-		if (!window.userModel){
-			window.userModel = new UserModel();
-			window.userModel.fetch(id, function(){
-				(new LayoutView({model : window.userModel})).render();
-			});
-		}
-	},
-
-	sandbox : function() {
-		var self = this;
-		(new SandBox()).render();
-	},
-
-	defaultAction : function(){
-	}
-});
-
-
-window.loadTemplate = function(name, next){
-	$.get('templates/' + name + '.html', function(data) {
-		next(data);
-	}, 'text');
-}
-
->>>>>>> commit nav bar
