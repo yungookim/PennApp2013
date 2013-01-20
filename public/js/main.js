9d818b3d9fddfb9f489e4a896928b37dbde38899
Yungoo Kim <yungookim@gmail.com>
2013-01-20 01:11:40 -0500

    bug fix

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,6 @@
 "use strict";
 $(function(){
 
-	FB.getLoginStatus(function(response) {
-		if (response.status === 'connected') {
-		  // the user is logged in and has authenticated your
-		  // app, and response.authResponse supplies
-		  // the user's ID, a valid access token, a signed
-		  // request, and the time the access token 
-		  // and signed request each expire
-		  var uid = response.authResponse.userID;
-		  var accessToken = response.authResponse.accessToken;
-		  console.log(uid + " " + accessToken);
-
-		} else {
-		  // the user isn't logged in to Facebook.
-
-		  alert('not logged in');
-
-		}
-	});
-
-
 	window.Templates = {};
 	window.userModel;
 	window.app = new AppRouter;
